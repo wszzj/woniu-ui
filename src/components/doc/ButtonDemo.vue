7827679c2d0ba071ff945e3340b0ba25a37e282c
zzj <1005978400@qq.com>
2023-12-22 00:12:33 +0800

    基础button样式

--- src/components/doc/ButtonDemo.vue
+++ src/components/doc/ButtonDemo.vue
@@ -1,13 +1,13 @@
 <template>
-  <Button @click="onClick" size="small">按钮</Button>
+  <Button theme="primary" size="small">主要按钮</Button>
+  <Button size="small">默认按钮</Button>
+  <Button theme="dashed" size="small">虚线按钮</Button>
+  <Button theme="text" size="small">文字按钮</Button>
+  <Button theme="link" size="small">链接按钮</Button>
 </template>
 
 <script setup lang="ts">
 import Button from "@/lib/Button.vue";
-
-const onClick = () => {
-  console.log("hi");
-};
 </script>
 
 <style lang="scss" scoped></style>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,19 +1,41 @@
 <template>
-  <div>
-    <button v-bind="$attrs"><slot></slot></button>
-  </div>
+  <button :class="{ [`theme-${props.theme}`]: props.theme }" class="wo-button">
+    <slot></slot>
+  </button>
 </template>
 
 <script lang="ts" setup>
-import { useAttrs } from "vue";
-
-defineOptions({
-  inheritAttrs: false,
+export interface Props {
+  theme: string;
+}
+const props = withDefaults(defineProps<Props>(), {
+  theme: "default",
 });
-const attrs = useAttrs();
-console.log(attrs);
-
-const emit = defineEmits();
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+$blue: #3a74f9;
+$lightBlue: #5993f9;
+.wo-button {
+  box-sizing: border-box;
+  font-size: 14px;
+  padding: 8px 15px;
+  border-radius: 6px;
+  margin-right: 16px;
+  margin-top: 16px;
+  display: inline-flex;
+  flex-direction: row;
+  white-space: nowrap;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  border: 1px solid #d9d9d9;
+  box-shadow: 0 1px 0 fade-out($color: #000000, $amount: 0.9);
+  cursor: pointer;
+  &:hover,
+  &:focus {
+    color: #4096ff;
+    border-color: #4096ff;
+  }
+}
+</style>
