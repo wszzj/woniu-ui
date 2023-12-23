4e84d238ef9d75662acb89e844fa653727508721
zzj <1005978400@qq.com>
2023-12-23 20:36:53 +0800

    Tabs css1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    <div v-for="(t, i) in titles" :key="i">{{ t }}</div>
-    <component v-for="(el, i) in defaults" :is="el" :key="i" />
+  <div class="wo-tabs">
+    <div class="wo-tabs-nav">
+      <div v-for="(t, i) in titles" :key="i" class="wo-tabs-item selected">
+        {{ t }}
+      </div>
+    </div>
+    <div class="wo-tabs-content">
+      <component v-for="(el, i) in defaults" :is="el" :key="i" />
+    </div>
   </div>
 </template>
 
@@ -25,4 +31,42 @@ const titles = defaults.map((element: VNode) => {
 });
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.wo-tabs {
+  > .wo-tabs-nav {
+    display: flex;
+    position: relative;
+    padding-bottom: 8px;
+    &::after {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      height: 1px;
+      width: 300px;
+      background-color: #d9d9d9;
+      z-index: 1;
+    }
+    > .wo-tabs-item {
+      margin-right: 16px;
+      position: relative;
+
+      &.selected {
+        &::after {
+          content: "";
+          position: absolute;
+          bottom: -8px;
+          left: 0;
+          height: 1px;
+          width: 30px;
+          background-color: red;
+          z-index: 2;
+        }
+      }
+    }
+  }
+  > .wo-tabs-content {
+    padding: 8px 0;
+  }
+}
+</style>
