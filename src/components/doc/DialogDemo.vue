47f2f7c0f240d9dad1a9ed5be0a793d3ef8bc128
zzj <1005978400@qq.com>
2023-12-23 14:16:41 +0800

    创建dialog

--- src/components/doc/DialogDemo.vue
+++ src/components/doc/DialogDemo.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>Dialog组件</div>
+  <div>dialog示例</div>
+  <h1>示例1</h1>
+  <Dialog></Dialog>
 </template>
 
-<script>
-export default {};
+<script setup lang="ts">
+import Dialog from "@/lib/Dialog.vue";
 </script>
 
 <style></style>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wo-dialog-overlay"></div>
+  <div class="wo-dialog-wrapper">
+    <header>
+      <span class="title">标题</span>
+      <span class="close">x</span>
+    </header>
+    <main>内容</main>
+    <footer>
+      <Button>确认</Button>
+      <Button>取消</Button>
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Button from "./Button.vue";
+</script>
+
+<style lang="scss"></style>
