0b6173c1c3273f47c60ed903fa47069f12ada2ec
zzj <1005978400@qq.com>
2023-12-27 20:59:33 +0800

    添加介绍和开始页面内容

--- src/views/GetStart.vue
+++ src/views/GetStart.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>howtouse</div>
+  <h1>安装</h1>
+  <p>
+    打开终端运行以下命令
+    <br />
+   <pre> <code>npm install woniu-ui</code></pre>
+    或者
+    <br />
+
+    <pre><code>yarn install woniu-ui</code></pre>
+  </p>
 </template>
 
 <script setup></script>
--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>intro</div>
+  <h1>介绍</h1>
+  <p>
+    蜗牛UI是基于 Vue3 和 TypeScript 实现的一套 UI
+    组件库，它提供了一些的UI组件，可以减少用户对常用组件的封装，降低了开发的难易程度。
+  </p>
+  <p>
+    蜗牛UI是我学习 Vue3
+    所练习的一个小项目，全部亲手编写，尽量不使用第三方库，所以强烈不建议你将此
+    UI 库用于生产环境。源代码放在了 GitHub
+    上。你也可以直接查看每个组件的源代码和示例，运行方法见 README.md。
+  </p>
 </template>
 
 <script setup></script>
