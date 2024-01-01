6ef9a2dc03d7e761611b5652b7c09e1ee4cbc908
zzj <1005978400@qq.com>
2024-01-01 10:47:34 +0800

    Button全局组件，修改a标签

---
+++ src/components/doc/Button1Demo.vue
@@ -0,0 +1,14 @@
+<template>
+  <Button>实线按钮</Button>
+  <Button theme="dashed">虚线按钮</Button>
+  <Button theme="text">文字</Button>
+  <Button theme="link">链接</Button>
+</template>
+<script setup lang="ts">
+import Button from "@/lib/Button.vue";
+</script>
+<style lang="scss" scoped>
+Button {
+  margin: 0 8px;
+}
+</style>
