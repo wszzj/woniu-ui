cb2e4a9e967ff5de273102948af971778bf37c08
zzj <1005978400@qq.com>
2024-01-01 21:50:41 +0800

    rollup配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 import path from "path";
 import svgLoader from "vite-svg-loader";
 import Markdown from "vite-plugin-md";
@@ -15,6 +16,23 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/lib/main.ts"),
+      name: "woniu-ui",
+      fileName: (format) => `woniu-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue", "vite-plugin-md", "vite-svg-loader"],
+      output: {
+        globals: {
+          vue: "Vue",
+          "vite-plugin-md": "vitePluginMd",
+          "vite-svg-loader": "viteSvgLoader",
+        },
+      },
     },
   },
 });
